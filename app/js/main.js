5a4f64a7373d12a1ec06e5abc6303964f82bc5af
Kiersten <kiersten.schmidt@nbcuniversal.com>
2019-07-11 12:04:25 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -22,36 +22,54 @@ document.addEventListener('DOMContentLoaded', function () {
         title: {
             text: null
         },
-        data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
+        series: [{
+            data: [
+                ["$30,000",252600],
+                ["$40,000",387900],
+                ["$50,000",523500],
+                ["$60,000",659100],
+                ["$70,000",794700],
+                ["$80,000",930300],
+                ["$90,000",1065600],
+                ["$100,000",1204800],
+                ["$150,000",2024400],
+                ["$200,000",2896500]
+            ],
+        }],
+        plotOptions: {
+            series: {
+                groupPadding: .1
+            }
         },
         legend: {
-            align: 'right',
-            symbolRadius: 0,
-            verticalAlign: 'top',
-            x: 10,
-            itemMarginTop: -10
+            enabled: false
         },
         xAxis: {
             labels: {
                 style: {
                     whiteSpace: 'nowrap'
                 }
-            }
+            },
+            type: 'category'
         },
         yAxis: {
             title: false,
             labels: {
                 useHTML: true,
-                overflow: 'allow'
-            }
+                overflow: 'allow',
+                formatter: function () {
+                    return (this.value)/1000000;
+                }
+            },
+            max: 3000000
         },
         credits: {
             enabled: false
         },
         tooltip: {
             shadow: false,
-            padding: 10
+            padding: 10,
+            pointFormat: '<b>Savings needed:</b> ${point.y}<br>'
         },
         responsive: {
             rules: [{
